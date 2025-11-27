585f4d1c043d639c0745f4cc4254bfb174e91a8e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-11-28 06:29:50 +0900

    Start trying to replicate the builtin romaji input's handling

--- japanese-kana.el
+++ japanese-kana.el
@@ -1,4 +1,4 @@
-;;; japanese-kana.el --- Quail package for typing Japanese with かな入力 -*- lexical-binding: t; -*-
+;;; japanese-kana.el --- Quail package for typing Japanese using Kana layout -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2025 Kisaragi Hiu
 ;;
@@ -21,6 +21,8 @@
 (require 'cl-lib)
 (require 'quail)
 
+;; We want the values to be strings even when they can be characters, to make it
+;; easy to convert them to katakana.
 (defvar japanese-kana-rules
   (append
    '(("4%" ["うぇ" "ゑ"])
@@ -63,13 +65,54 @@
                              "つさそひこみもねるめ"
                              "っさそひこみも、。・"))))))
 
+(defun japanese-kana-update-translation (control-flag)
+  "Update Quail translation region for Kana input.
+See `quail-update-translation' for what CONTROL-FLAG would be."
+  (if (null control-flag)
+      (setq quail-current-str
+            (if (/= (aref quail-current-key 0) ?q)
+                (or quail-current-str quail-current-key)
+              ""))
+    (if (integerp control-flag)
+        (let ((keylen (length quail-current-key)))
+          (cond ((= control-flag 0)
+                 (setq quail-current-str (aref quail-current-key 0)
+                       control-flag t))
+                ((= (aref quail-current-key 0) ?n)
+                 (setq quail-current-str ?ん)
+                 (if (and quail-japanese-use-double-n
+                          (> keylen 0)
+                          (= (aref quail-current-key 1) ?n))
+                     (setq control-flag t)))
+                ((and (> keylen 1)
+                      (= (aref quail-current-key 0) (aref quail-current-key 1)))
+                 (setq quail-current-str ?っ))
+                (t
+                 (setq quail-current-str (aref quail-current-key 0))))
+          (if (integerp control-flag)
+              (setq unread-command-events
+                    (append
+                     (substring quail-current-key control-flag)
+                     unread-command-events)))))))
+
 (quail-define-package
  "japanese-kana-hiragana" "Japanese" "かな" t
- "Japanese input method for typing Hiragana with the かな入力 scheme."
- nil t nil)
+ "Japanese input method with Kana layout.
+
+The goal is that this skips the Roman-Kana transliteration step in the
+builtin \"japanese\" romaji input method with the Kana layout, then does
+Kana-Kanji conversion after."
+ nil t t
+ nil nil nil nil nil
+ #'japanese-kana-update-translation
+ '(("K" . quail-japanese-toggle-kana)
+   (" " . quail-no-conversion)
+   ("\C-m" . quail-no-conversion)
+   ([return] . quail-no-conversion)))
 
 (dolist (it japanese-kana-rules)
   (quail-defrule (car it) (cdr it)))
+
 
 (quail-define-package
  "japanese-kana-katakana" "Japanese" "カナ" t
